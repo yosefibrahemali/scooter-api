2a6f12676134558b29671e45049f712343502715
yosefibrahemali <gargumy1@gmail.com>
2025-03-31 10:02:08 +0200

    Your commit message describing the changes

--- app/Http/Livewire/ActiveScooters.php
+++ app/Http/Livewire/ActiveScooters.php
@@ -7,8 +7,11 @@ use Livewire\Component;
 
 class ActiveScooters extends Component
 {
+
+    public $scooters ;
     public function render()
     {
-        $scooters = ScooterConnection::whereNull('disconnected_at')->get();
-        return view('livewire.active-scooters', compact('scooters'));    }
+        $this->scooters = ScooterConnection::whereNull('disconnected_at')->get();
+        return view('livewire.active-scooters', compact('scooters'));
+    }
 }
