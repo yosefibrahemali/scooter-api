942431445a3d87c4d2c33190ec1fd92fcabf21b0
yosefibrahemali <gargumy1@gmail.com>
2025-04-03 00:01:40 +0200

    تحديث الكود لإضافة ميزة جدة

--- app/Console/Commands/TcpServerCommand.php
+++ app/Console/Commands/TcpServerCommand.php
@@ -7,12 +7,19 @@ use App\Services\TcpServerService;
 
 class TcpServerCommand extends Command
 {
-    protected $signature = 'tcp:server';
+    protected $signature = 'tcp:server {port?}';
     protected $description = 'Start TCP server for scooter communication';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
     public function handle()
     {
-        $port = 5000;
+        $port = $this->argument('port') ?? 5000;
+        
+        $this->info("Starting TCP server on port {$port}...");
         
         $server = new TcpServerService();
         $server->start($port);
